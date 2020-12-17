2e3c550d7dc1639796d10d871d080abb757a7495
SShatzkin <sshatzkin@uchicago.edu>
2020-12-16 19:56:51 -0500

    Uploading to the database is working

--- public/scripts/image_loading.js
+++ public/scripts/image_loading.js
@@ -1,6 +1,39 @@
 // Get a reference to the storage service, which is used to create references in your storage bucket
 var storage = firebase.storage();
-console.log(storage);
 
 var storageRef = firebase.storage().ref();
 console.log(storageRef);
+
+// Listen for image uploaded
+const actualBtn = document.getElementById('file_input');
+
+const fileChosen = document.getElementById('uploadSpan');
+
+actualBtn.addEventListener('change', function(){
+  //fileChosen.textContent = "File: " + this.files[0].name;
+  console.log(this.files[0]);
+
+  var file = this.files[0];
+
+  var image_ref = storageRef.child('user_videos/' + file.name);
+  
+
+  image_ref.put(file).then(function(snapshot) {
+    console.log('Uploaded a file!');
+    alert("File uploaded!");
+  });
+
+  // Create file metadata including the content type
+  var metadata = {
+    contentType: file.type,
+    user: "UserNameGoesHere"
+  };
+
+  // Upload the file and metadata
+  var uploadTask = storageRef.child('user_videos/' + file.name).put(file, metadata);
+
+  
+
+})
+
+
